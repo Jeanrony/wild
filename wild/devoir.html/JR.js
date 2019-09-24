958cbd7fa4ff3a8d2b039920171c5b78f008f58c
simplonco <jean@gmail.com>
2019-09-24 10:24:53 +0200

    JS

---
+++ wild/devoir.html/JR.js
@@ -0,0 +1,22 @@
+//const firstName = "JR";
+console.Log(typeof firstName);
+
+let age = 23;
+console.Log(typeof age);
+
+let wilde = false;
+console.Log(typeof wilde);
+
+const ecole = "wilde code school";
+console.Log(typeof ecole);
+
+wilde = true;
+age = 23 + 1;
+
+console.Log(firstName);
+console.Log(age);
+console.Log(ecole)
+
+for(let i = 0; i = 100; i = i + 5){
+    console.log("i")
+}
---
+++ wild/devoir.html/devoir.js
@@ -0,0 +1,6 @@
+const movie = "Green book";
+let year = "2019";
+const month = "Avril";
+const message = "Best " + movie + "coming in " + year + "in" + month ;
+
+alert(message);
